8416c7510f7673d4a137fe757e0938d3ec9229c2
MasterSun8 <kacpi.doro@gmail.com>
2023-08-20 16:27:30 +0200

    XD

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const activation = (x) => x > 0 ? x : 0
 function randomArray(size) {
     let arr = new Array()
     for (let i = 0; i < size; i++) {
-        arr.push(Math.round(Math.random() - 0.5))
+        arr.push(Math.random() - 0.5)
     }
     return arr
 }
@@ -24,7 +24,7 @@ function dotProd(arr1, arr2) {
 function createBiases(count) {
     let biases = new Array()
     for (let c = 0; c < count; c++) {
-        biases.push(Math.random() * 5)
+        biases.push(Math.random() * 2)
     }
     return biases
 }
@@ -44,13 +44,24 @@ function makeLayers(...args) {
         layers.push(createLayer(before, count))
         before = count
     })
-    args.pop()
     args.shift()
     let biases = new Array()
     args.forEach(x => {
         biases.push(createBiases(x))
     })
     return [biases, layers]
+}
+
+function calculateLayer(input, output, biases) {
+    /*
+
+    input = 784x1
+    output = 15x784
+    biases = 15x1
+
+    /**/
+
+    // for(let i = 0; i<output.length)
 }
 
 async function csvObject(name) {
@@ -74,6 +85,7 @@ async function csvObject(name) {
 }
 
 async function main() {
+    console.time("LoadCSV")
     //let [trainVal, trainArray] = csvObject('train')
     let [testVal, testArray] = await csvObject('test')
     const size = testArray[0].length
@@ -81,17 +93,57 @@ async function main() {
 
     let num = process.argv[2]
     val = num = num ? num : 0
+    console.timeEnd("LoadCSV")
 
     /*
     console.log(val ? testVal[num] : '')
     console.log(testArray[num])
     /**/
+
+    console.time("Layer creation")
     let [biases, layers] = makeLayers(size, 15, 10)
+    console.timeEnd("Layer creation")
     //console.log(biases)
     fs.writeFile('nodes.txt', JSON.stringify(layers))
     fs.writeFile('biases.txt', JSON.stringify(biases))
 
+
+    let tee = 0
+    let layer = 1
+
+    calculateLayer(layers[layer-1], layers[layer], biases[layer-1])
+/*
+    let res = new Array()
+    let biggest = 0
+    for (let ind = 0; ind < layers[layer].length; ind++) {
+        tee = 0
+        node = layers[layer][ind]
+        for (let index = 0; index < node.length; index++) {
+            tee += (node[index] * layers[layer - 1][index])
+        }
+        let r = activation(tee + biases[layer - 1][ind])
+        biggest = r > biggest ? r : biggest
+        res.push(r)
+    }
+    console.table(res)
+
+    console.log(biggest)
+
+    /*
+        console.log('')
+        res = new Array()
     
+        layers[layer].forEach((node, ind) => {
+            tee = 0
+            node.forEach((prod, index) => {
+                tee += (prod * layers[layer - 1][index])
+            })
+            let r = activation(tee + biases[layer - 1][ind])
+            biggest = r>biggest ? r : biggest
+            res.push(r)
+        })
+        console.table(res)
+    /**/
 }
 
 /*    
@@ -113,4 +165,7 @@ async function main() {
     console.log(string)
 /**/
 
-main()
+console.time("Program")
+main().then(() => {
+    console.timeEnd("Program")
+})
