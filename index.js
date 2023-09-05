75e1dd722a7a32a4a9e5586fd7dc931b4e2a98d0
MasterSun8 <kacpi.doro@gmail.com>
2023-09-05 11:04:20 +0200

    changed to OOP

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const fs = require("fs/promises")
-
-const activation = (x) => x > 0 ? x : 0
+const NeuralNetwork = require("./neural-network")
 
 async function csvObject(name) {
     let arr = new Array()
@@ -16,81 +15,6 @@ async function csvObject(name) {
     return [val, arr]
 }
 
-function randomArray(size) {
-    let arr = new Array()
-    for (let i = 0; i < size; i++) {
-        arr.push(Math.random() - 0.5)
-    }
-    return arr
-}
-
-function createBiases(count) {
-    let biases = new Array()
-    for (let c = 0; c < count; c++) {
-        biases.push(Math.random() - 0.2)
-    }
-    return biases
-}
-
-function createLayer(conns, nodes) {
-    let layer = new Array()
-    for (let c = 0; c < nodes; c++) {
-        layer.push(randomArray(conns))
-    }
-    return layer
-}
-
-function makeLayers(...args) {
-    let before = args.shift()
-    let layers = new Array()
-    args.forEach(count => {
-        layers.push(createLayer(before, count))
-        before = count
-    })
-    let biases = new Array()
-    args.forEach(x => {
-        biases.push(createBiases(x))
-    })
-    return [biases, layers]
-}
-
-function calculateLayer(input, output, biases) {
-    let inputSize = input.length
-    if (input.length != output[0].length) {
-        throw new Error(`The layer size is not equal: expected ${output[0].length} in the first layer but it's ${inputSize}`)
-    }
-
-    let finalOutput = new Array()
-    let sum = 0
-
-    for (let i = 0; i < output.length; i++) {
-        let result = 0
-        for (let index = 0; index < inputSize; index++) {
-            result += output[i][index] * input[index]
-        }
-        result = activation(result + biases[i])
-        sum += result
-        finalOutput.push(result)
-    }
-
-    finalOutput = finalOutput.map((x) => {
-        return x / sum
-    })
-    return finalOutput
-}
-
-function recognizeNumber(input, layers, biases, trueValue) {
-    let lay = calculateLayer(input, layers[0], biases[0])
-    let layer = 1
-    for (; layer < layers.length; layer++) {
-        lay = calculateLayer(lay, layers[layer], biases[layer])
-    }
-    let result = lay.indexOf(Math.max(...lay))
-    console.table(lay)
-    console.log(`The result is: ${result} expected ${trueValue}`)
-    return lay
-}
-
 async function main() {
     console.time("LoadCSV")
     //let [trainVal, trainArray] = csvObject('train')
@@ -98,15 +22,16 @@ async function main() {
     const size = testArray[0].length
     console.timeEnd("LoadCSV")
 
+
+
     console.time("Layer creation")
-    let [biases, layers] = makeLayers(size, 20, 10)
+    let network = new NeuralNetwork(size, 20, 10)
     console.timeEnd("Layer creation")
-    fs.writeFile('nodes.txt', JSON.stringify(layers))
-    fs.writeFile('biases.txt', JSON.stringify(biases))
+    fs.writeFile('nodes.txt', JSON.stringify(network.layers))
+    fs.writeFile('biases.txt', JSON.stringify(network.biases))
 
     console.time('Single network calculation')
-    let output = recognizeNumber(testArray[0], layers, biases, testVal[0])
-
+    let output = network.recognizeNumber(testArray[0], testVal[0])
     console.timeEnd('Single network calculation')
 }
 
---
+++ neural-network.js
@@ -0,0 +1,101 @@
+class NeuralNetwork {
+    constructor(...args) {
+        if (args[0] === true) {
+            this.layers = args[1]
+            this.biases = args[2]
+        } else {
+            [this.biases, this.layers] = this.makeLayers(...args)
+        }
+    }
+
+    static activation(x) {
+        return x > 0 ? x : 0
+    }
+
+    randomArray(size) {
+        let arr = new Array()
+        for (let i = 0; i < size; i++) {
+            arr.push(Math.random() - 0.5)
+        }
+        return arr
+    }
+
+    createBiases(count) {
+        let biases = new Array()
+        for (let c = 0; c < count; c++) {
+            biases.push(Math.random() - 0.2)
+        }
+        return biases
+    }
+
+    createLayer(conns, nodes) {
+        let layer = new Array()
+        for (let c = 0; c < nodes; c++) {
+            layer.push(this.randomArray(conns))
+        }
+        return layer
+    }
+
+    makeLayers(...args) {
+        let before = args.shift()
+        let layers = new Array()
+        args.forEach(count => {
+            layers.push(this.createLayer(before, count))
+            before = count
+        })
+        let biases = new Array()
+        args.forEach(x => {
+            biases.push(this.createBiases(x))
+        })
+        return [biases, layers]
+    }
+
+    calculateLayer(input, output, biases) {
+        let inputSize = input.length
+        if (input.length != output[0].length) {
+            throw new Error(`The layer size is not equal: expected ${output[0].length} in the first layer but it's ${inputSize}`)
+        }
+
+        let finalOutput = new Array()
+        let sum = 0
+
+        for (let i = 0; i < output.length; i++) {
+            let result = 0
+            for (let index = 0; index < inputSize; index++) {
+                result += output[i][index] * input[index]
+            }
+            result = this.constructor.activation(result + biases[i])
+            sum += result
+            finalOutput.push(result)
+        }
+
+        finalOutput = finalOutput.map((x) => {
+            return x / sum
+        })
+        return finalOutput
+    }
+
+    forwardProp(input) {
+        let lay = this.calculateLayer(input, this.layers[0], this.biases[0])
+        for (let layer = 1; layer < this.layers.length; layer++) {
+            lay = this.calculateLayer(lay, this.layers[layer], this.biases[layer])
+        }
+        return lay
+    }
+
+    recognizeNumber(input, trueValue = false) {
+        let lay = this.forwardProp(input, this.layers, this.biases)
+        let result = lay.indexOf(Math.max(...lay))
+        if (trueValue) {
+            console.log(`The result is: ${result} expected ${trueValue}`)
+        }
+        return result
+    }
+
+    backProp() {
+
+    }
+
+}
+
+module.exports = NeuralNetwork
